26a8cd12a6229f0cf2cda208ef6c026ea811d45c
sf <90348284+exterror@users.noreply.github.com>
2021-10-11 15:45:38 +0300

    new version

---
+++ script.js
@@ -0,0 +1,16 @@
+$("p").attr("style", "text-align: center; color: grey; font-size: 18px; padding: 15px");
+
+$('.js-btn-modal').on('click', function () {
+  $('#overlay').fadeIn(3);
+  let id = $(this).data('id');
+  $('.js-modal[data-id="modal' + id + '"]').fadeIn(3);
+});
+
+$('.js-close-btn').on('click', function () {
+  $('#overlay').fadeOut(3);
+  $('.js-modal').fadeOut(3);
+});
+$('#overlay').on('click', function () {
+  $('#overlay').fadeOut(3);
+  $('.js-modal').fadeOut(3);
+});
